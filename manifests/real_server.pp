5cdffc8312b2b393703689b06d84fa8066d977cd
Raffael Schmid <raf@nine.ch>
2012-08-15 16:50:54 +0200

    add some more parameters

--- manifests/real_server.pp
+++ manifests/real_server.pp
@@ -5,8 +5,23 @@ define keepalived::real_server(
   $weight = 10,
   $check_type,
   $connect_timeout = 4,
+  $inhibit_on_failure = false,
+
+  $delay_before_retry = false,
+  $connect_timeout = false,
+  $bind_to = false,
+
+  $url_path = false,
+  $url_digest = false,
+  $url_status_code = false,
+
+  $connect_port = false,
+
   $helo_name = "loadbalancer.healthcheck.local"
 ) {
+
+  validate_bool($inhibit_on_failure)
+  validate_re($check_type, ['HTTP', 'SSL', 'TCP', 'SMTP'])
 
   concat::fragment {
     "keepalived.virtual_server.${virtual_server_name}.${virtual_server_port}.real_server.${name}":
